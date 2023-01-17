5c98dc94635b34d0904a3c22b63316bb4c3e22f4
ZAndreyV <119517277+ZAndreyV@users.noreply.github.com>
2023-01-17 21:59:41 +0700

    Added Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+int[] array = CreatArrayRndInt(4, -10, 9);
+PrinArray(array);
+int sumOddElementsOfArray = SumOddElementsOfArray(array);
+Console.WriteLine(sumOddElementsOfArray);
+
+int[] CreatArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);       
+    }
+    return arr;
+}
+
+void PrinArray(int[] arr)
+{
+    Console.Write("Sum of odd elements in array [");
+    for ( int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("] -> ");
+}
+
+
+int SumOddElementsOfArray(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum += arr[i];
+    }
+    return sum;
+}
